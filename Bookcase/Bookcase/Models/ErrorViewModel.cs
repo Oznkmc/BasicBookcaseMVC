73981d672f2721ba8f9b751e895ec2c3a1789fef
Ozan <oznkmc21@gmail.com>
2026-05-12 21:48:59 +0300

    Kategori Yönetim Sistemini bitirdim ve entegre ettim. Öğrenerek ilerlemeye devam.

--- Bookcase/Bookcase/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Bookcase.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- Bookcase/Models/ErrorViewModel.cs
+++ Bookcase/Models/ErrorViewModel.cs
@@ -1,9 +1,10 @@
+using System;
+
 namespace Bookcase.Models
 {
     public class ErrorViewModel
     {
         public string? RequestId { get; set; }
-
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
 }
